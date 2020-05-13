9dc58f4de13ab4c3937880ded1f9202eb26a4068
ArakiTakaki <araki_takaki@yahoo.co.jp>
2020-05-13 10:55:48 +0900

    update testcase

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testRegex: "(/__tests__/.*|(\\.|/)(spec))\\.(jsx?|tsx?)$",
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.test.json'
-    }
-  },
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
