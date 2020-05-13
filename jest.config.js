2a25b12c8b311326e8e4eb609640bbba30a4db27
ArakiTakaki <araki_takaki@yahoo.co.jp>
2020-05-13 10:55:04 +0900

    update testcase

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testRegex: "(/__tests__/.*|(\\.|/)(spec))\\.(jsx?|tsx?)$",
   testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json'
+    }
+  },
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
--- packages/javascript-slider/index.ts
+++ packages/javascript-slider/index.ts
@@ -1,6 +1,4 @@
 import { Slider }  from './lib/Slider'
-console.log('hello');
-
 export default {
   Slider,
 };
