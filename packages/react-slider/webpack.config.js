1795f77884a13fb6d8b69ce2a89ed435515739e8
ArakiTakaki <arakitakaki.work@gmail.com>
2020-11-07 16:57:56 +0900

    update

--- packages/react-slider/webpack.config.js
+++ packages/react-slider/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    index: [path.resolve(__dirname, './example/index.tsx')],
+    index: [path.resolve(__dirname, "./example/index.tsx")],
   },
   output: {
-    path: path.resolve(__dirname, './example/dist'),
-    publicPath: '/',
-    filename: '[name].js',
+    path: path.resolve(__dirname, "./example/dist"),
+    publicPath: "/",
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         options: {
           appendTsSuffixTo: [/\.vue$/],
         },
@@ -23,18 +23,18 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: [".js", ".ts", ".tsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, './example/index.html'),
+      filename: "index.html",
+      template: path.resolve(__dirname, "./example/index.html"),
       hash: true,
       inject: true,
     }),
   ],
   devServer: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     disableHostCheck: true,
     hot: true,
     inline: true,
--- packages/slider-core/webpack.config.js
+++ packages/slider-core/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    index: [path.resolve(__dirname, './example/index.ts')],
+    index: [path.resolve(__dirname, "./example/index.ts")],
   },
   output: {
-    path: path.resolve(__dirname, './example/dist'),
-    publicPath: '/',
-    filename: '[name].js',
+    path: path.resolve(__dirname, "./example/dist"),
+    publicPath: "/",
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         options: {
           appendTsSuffixTo: [/\.vue$/],
         },
@@ -23,22 +23,23 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: [".js", ".ts", ".tsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, './example/index.html'),
+      filename: "index.html",
+      template: path.resolve(__dirname, "./example/index.html"),
       hash: true,
       inject: true,
     }),
   ],
   devServer: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     disableHostCheck: true,
     hot: true,
     inline: true,
     overlay: true,
-    port: 4030
+    port: 4030,
   },
 };
+
